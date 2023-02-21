b8203c6969dc8fc0811f170fcb206924c321a775
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-02-21 16:24:52 +0100

    Change the two with circles in Chats.tsx

--- components/Chats.tsx
+++ components/Chats.tsx
@@ -1,4 +1,4 @@
-import {Pressable, ScrollView, Text, TextInput, View} from "react-native";
+import {Image, Pressable, ScrollView, Text, TextInput, TouchableOpacity, View} from "react-native";
 import styles from "../styles/mainstyle";
 import {MsgBox} from "./MsgBox";
 
@@ -9,17 +9,23 @@ const Chats = () => {
             <View style={styles.spacer}></View>
             <View style={styles.header}>
                 <View style={styles.bubbleButtonRight}>
-                    <Pressable ></Pressable>
+                    <TouchableOpacity >
+                        <Text style={styles.bubbleButtonRightImg}>Edit</Text>
+                    </TouchableOpacity>
                 </View>
                 <Text style={styles.textH1Style}>Chats</Text>
                 <View style={styles.bubbleButtonLeft}>
-                    <Pressable></Pressable>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/icons/edit.png')} style={styles.bubbleButtonLeftImg}></Image>
+                    </TouchableOpacity>
                 </View>
             </View>
             <View style={styles.searchBox}>
                 <TextInput placeholder="Search"></TextInput>
             </View>
             <ScrollView>
+                <MsgBox lastMsg="Test Length of message before breaking everything. Test Length of message before breaking everything. Test Length of message before breaking everything" nameOfUser="John" date="12:00" />
+                <MsgBox lastMsg="Test Length of message before breaking everything. Test Length of message before breaking everything. Test Length of message before breaking everything" nameOfUser="John" date="12:00" />
                 <MsgBox lastMsg="Test Length of message before breaking everything. Test Length of message before breaking everything. Test Length of message before breaking everything" nameOfUser="John" date="12:00" />
             </ScrollView>
         </View>
