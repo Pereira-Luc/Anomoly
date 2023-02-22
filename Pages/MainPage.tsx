faaf95dced40a417bb3d6d40281f9be12cfc37eb
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-02-22 17:40:36 +0100

    Add the menu to Take/Select pic for profile

--- Pages/MainPage.tsx
+++ Pages/MainPage.tsx
@@ -17,28 +17,31 @@ import Groups from "../components/Groups";
 import Settings from "../components/Settings";
 
 import React, {useState} from "react";
+import {ActionSheetProvider} from "@expo/react-native-action-sheet";
 
 const MainPage = () => {
     //Default page is Chats
     const [page, setPage] = React.useState("Chats");
 
     return (
-        <View style={styles.mainContainer}>
-            {page === "Chats" ? <Chats /> : null}
-            {page === "Groups" ? <Groups /> : null}
-            {page === "Settings" ? <Settings /> : null}
-            <View style={styles.footer}>
-                <TouchableOpacity style={[styles.groupsButtonFooter, styles.footerButton]} onPress={() => setPage("Groups")}>
-                    <Image source={require('../assets/icons/group1.png')} style={[styles.footerImg,  page === "Groups" && styles.footerImgActive]}></Image>
-                </TouchableOpacity>
-                <TouchableOpacity style={[styles.chatsButtonFooter, styles.footerButton]} onPress={() => setPage("Chats")}>
-                    <Image source={require('../assets/icons/chat1.png')} style={[styles.footerImg,  page === "Chats" && styles.footerImgActive]}></Image>
-                </TouchableOpacity>
-                <TouchableOpacity style={[styles.settingButtonFooter, styles.footerButton]} onPress={() => setPage("Settings")}>
-                    <Image source={require('../assets/icons/setting3.png')} style={[styles.footerImg, page === "Settings" && styles.footerImgActive]}></Image>
-                </TouchableOpacity>
-            </View>
-        </View >
+        <ActionSheetProvider>
+            <View style={styles.mainContainer}>
+                {page === "Chats" ? <Chats /> : null}
+                {page === "Groups" ? <Groups /> : null}
+                {page === "Settings" ? <Settings /> : null}
+                <View style={styles.footer}>
+                    <TouchableOpacity style={[styles.groupsButtonFooter, styles.footerButton]} onPress={() => setPage("Groups")}>
+                        <Image source={require('../assets/icons/group1.png')} style={[styles.footerImg,  page === "Groups" && styles.footerImgActive]}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.chatsButtonFooter, styles.footerButton]} onPress={() => setPage("Chats")}>
+                        <Image source={require('../assets/icons/chat1.png')} style={[styles.footerImg,  page === "Chats" && styles.footerImgActive]}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.settingButtonFooter, styles.footerButton]} onPress={() => setPage("Settings")}>
+                        <Image source={require('../assets/icons/setting3.png')} style={[styles.footerImg, page === "Settings" && styles.footerImgActive]}></Image>
+                    </TouchableOpacity>
+                </View>
+            </View >
+        </ActionSheetProvider>
     )
 }
 
--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,9 +1,86 @@
-import {Image, Text, TouchableOpacity, View} from "react-native";
+import {Image, Platform, Text, TouchableOpacity, View} from "react-native";
 import styles from "../styles/mainstyle";
 import SettingsBox from "./SettingsBox";
 
+import {ActionSheetProvider, useActionSheet} from '@expo/react-native-action-sheet';
+import * as ImagePicker from 'expo-image-picker';
+
+
+
 
 const Settings = () => {
+    const checkPermissions = async (cameraGranted = null, status = null) => {
+        if (!cameraGranted) {
+            alert('You need to enable camera permissions to work');
+            return;
+        }
+        if (status !== 'granted' && status !== null) {
+            alert('You need to enable gallery permissions to work');
+            return;
+        }
+        return true;
+    }
+
+    const { showActionSheetWithOptions } = useActionSheet();
+    const showPicMenu = async () => {
+
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        const { granted: cameraGranted } = await ImagePicker.getCameraPermissionsAsync();
+
+        const options = ['Take Photo', 'Choose From Gallery', 'Cancel'];
+        const cancelButtonIndex = 2;
+        const takePhotoIndex = 0;
+        const chooseFromGalleryIndex = 1;
+
+        showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonIndex,
+                userInterfaceStyle: 'dark',
+            },
+            (selectedIndex: number) => {
+                switch (selectedIndex) {
+                    case takePhotoIndex:
+                        // Take Photo
+                        //Check permissions
+                        if (!checkPermissions(cameraGranted)) return;
+
+                        //Open camera
+                        ImagePicker.launchCameraAsync({
+                            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                            allowsEditing: true,
+                            aspect: [4, 3],
+                            quality: 0.5,
+                        }).then((result) => {
+                            if (result.canceled) return;
+                            console.log(result);
+                        });
+                        break;
+                    case chooseFromGalleryIndex:
+                        // Choose From Gallery
+                        //Check permissions
+                        if (!checkPermissions(status)) return;
+
+                        //Open gallery
+                        ImagePicker.launchImageLibraryAsync({
+                            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                            allowsEditing: true,
+                            aspect: [4, 3],
+                            quality: 0.5,
+                        }).then((result) => {
+                            if (result.canceled) return;
+                            console.log(result);
+                        });
+                        break;
+                    case cancelButtonIndex:
+                        // Cancel
+                        console.log('Cancel');
+                        break;
+                }
+            }
+        )
+    };
+
     return (
         <View style={styles.mainContainer}>
             <View style={styles.spacer}></View>
@@ -11,17 +88,16 @@ const Settings = () => {
                 <Text style={[styles.textH1Style]}>Settings</Text>
             </View>
             <View style={styles.profileImageEdit}>
-                <TouchableOpacity style={styles.profileImage}>
+                <TouchableOpacity style={styles.profileImage} onPress={() => showPicMenu()}>
                     <View style={styles.changeContainer}>
                         <Text style={styles.changeText}>Change</Text>
                     </View>
                 </TouchableOpacity>
-                <Text style={[styles.textH2Style,styles.marginTop5]}>Profile Name</Text>
+                <Text style={[styles.textH2Style, styles.marginTop5]}>Profile Name</Text>
             </View>
             <View style={styles.settingsContainer}>
                 <SettingsBox />
             </View>
-
         </View>
     )
 }
