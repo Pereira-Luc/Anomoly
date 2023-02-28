329b16170e1af984b9743a54a5c3d9f1588a0448
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-02-28 22:44:57 +0100

    switch colors

--- Pages/StartPage.tsx
+++ Pages/StartPage.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import {
-    Text,
-    KeyboardAvoidingView,
-    Image,
-    Platform
-} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import {Image, KeyboardAvoidingView, Platform, Text} from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 import LoginForm from "../components/LoginForm";
 import styles from "../styles/mainstyle";
 import SignUpForm from "../components/SignUpForm";
+
 const StartPage = () => {
     const [isLogin, setLogin] = React.useState(true);
 
     return (
-        <LinearGradient style={styles.container} colors={['#070d28', '#16002a','#200026','#1c0009']}>
+        <LinearGradient style={styles.container} colors={['#000000', '#001115', '#001213', '#001c14']}>
             <Image source={require('../assets/img/Anomoly.png')} style={styles.imgLogo}></Image>
-            <Text style={styles.heading1} >Anomoly</Text>
-            <KeyboardAvoidingView behavior={(Platform.OS === 'ios')? "position" : null} keyboardVerticalOffset={10} >
-                {isLogin ? <LoginForm setLogin={setLogin} /> : <SignUpForm setLogin={setLogin} />}
+            <Text style={styles.heading1}>Anomoly</Text>
+            <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? "position" : null} keyboardVerticalOffset={10}>
+                {isLogin ? <LoginForm setLogin={setLogin}/> : <SignUpForm setLogin={setLogin}/>}
             </KeyboardAvoidingView>
-        </LinearGradient >
+        </LinearGradient>
     )
 }
 
