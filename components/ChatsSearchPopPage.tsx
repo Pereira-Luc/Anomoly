6a3c02fcadacaf030b1e6b1b37da076b7b9fe9da
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-08 20:56:41 +0100

    Working search for new Friends

--- components/ChatsSearchPopPage.tsx
+++ components/ChatsSearchPopPage.tsx
@@ -2,32 +2,45 @@
 import React, {useMemo, useRef} from 'react';
 
 // import all the components we are going to use
-import {Platform, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Keyboard, Platform, StyleSheet, Text, TextInput, View} from 'react-native';
+
+import {FlatList} from 'react-native-gesture-handler';
 
 //import basic react native components
 import BottomSheet from '@gorhom/bottom-sheet';
 import {Portal} from "react-native-portalize";
 import styles from "../styles/mainstyle";
-
+import {colors} from "../styles/colors/colors";
+import {useQuery} from "@apollo/client";
+import {SEARCH_QUERY} from "../constants/graphql/querys/searchUser";
+import ProfileSearchBox from "./ProfileSearchBox";
 
 const ChatSearchPopPage = ({visible, setVisible}: any) => {
-
     const bottomSheetRef = useRef<BottomSheet>(null);
     const snapPoints = useMemo(() => ['90%', '90%'], []);
 
+    let [searchText, setSearchText] = React.useState("");
+
+
+    //Get Search Results from API SEARCH_QUERY
+    let {error, data} = useQuery(SEARCH_QUERY, {variables: {v: searchText}});
+
+
+    if (error) console.log(error.message);
     return (
         <Portal>
             <BottomSheet
                 ref={bottomSheetRef}
                 index={visible ? 1 : -1}
                 snapPoints={snapPoints}
-                onChange={(index) => setVisible(index !== -1)}
+                onChange={(index) => {
+                    setVisible(index !== -1)
+                    // Close the keyboard if the bottom sheet is closed
+                    if (index === -1) Keyboard.dismiss()
+                }}
                 enablePanDownToClose={true}
-                enableContentPanningGesture={true}
-                enableOverDrag={true}
-                backgroundStyle={{backgroundColor: '#151515'}}
+                backgroundStyle={{backgroundColor: colors.primaryBackgroundVeryDark,}}
                 handleIndicatorStyle={{backgroundColor: '#00ff9d'}}
-
                 style={chatSearchPop.bottomSheetStyle}>
                 {/*Bottom Sheet inner View*/}
                 <View style={chatSearchPop.bottomNavigationView}>
@@ -37,14 +50,20 @@ const ChatSearchPopPage = ({visible, setVisible}: any) => {
                         </Text>
                     </View>
                     <View style={chatSearchPop.body}>
+
                         <View style={[styles.searchBox, chatSearchPop.searchBox]}>
-                            <TextInput placeholderTextColor={"#a8a8a8"} placeholder="Search"
+                            <TextInput onChangeText={(v) => setSearchText(v)} placeholderTextColor={"#a8a8a8"}
+                                       placeholder="Search"
                                        style={styles.searchInput}></TextInput>
                         </View>
                         <View style={chatSearchPop.spacerLine}></View>
-                        <View style={{flex: 1, flexDirection: 'row'}}>
-
-                        </View>
+                        <FlatList
+                            data={data && data.searchUser}
+                            renderItem={({item, index}) => {
+                                //console.log("Iteams: " , item.userId);
+                                return (<ProfileSearchBox username={item.username} userID={item.userId}/>)
+                            }}
+                            keyExtractor={item => item.userId}/>
                     </View>
                 </View>
             </BottomSheet>
@@ -64,7 +83,6 @@ const chatSearchPop = StyleSheet.create({
 
     },
     bottomNavigationView: {
-        backgroundColor: '#151515',
         width: '100%',
         height: '90%',
         justifyContent: 'center',
@@ -72,20 +90,17 @@ const chatSearchPop = StyleSheet.create({
     },
     bottomSheetStyle: {
         backgroundColor: 'rgba(38,38,38,0)',
-
     },
     headerText: {
         textAlign: 'center',
         padding: 10,
         fontSize: (Platform.OS === 'ios') ? 20 : 17,
-        color: '#ffffff'
+        color: '#ffffff',
+        marginTop: 20,
     }, header: {
         flexDirection: 'column',
         justifyContent: 'space-between',
-    }, body: {
-        flex: 1,
-        backgroundColor: '#151515',
-    },
+    }, body: {},
     searchBox: {
         minWidth: '85%',
     }, spacerLine: {
