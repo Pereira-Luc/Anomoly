4e0e2072ecc98c1a994b70f68e4ef3b8fb87df3b
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-09 02:24:26 +0100

    add Search option and add as friend / accept

--- components/ProfileSearchBox.tsx
+++ components/ProfileSearchBox.tsx
@@ -1,10 +1,51 @@
-import {Image, Text, TouchableOpacity, View} from "react-native";
+import {ActivityIndicator, Image, Text, TouchableOpacity, View} from "react-native";
 import profileSearchBox from "../styles/profileSearchBox";
+import {useMutation} from "@apollo/client";
+import {ACCEPT_FRIEND_REQUEST_QUERY, ADD_FRIEND_QUERY} from "../constants/graphql/querys/addFriendQuery";
+import React, {useState} from "react";
+import * as SecureStore from "expo-secure-store";
+import {AuthPayload} from "../interfaces/AuthPayload";
 
-const ProfileSearchBox = ({userID, username}: any) => {
+const ProfileSearchBox = ({username, friendRequestStatus}: any) => {
 
-    const addUser = (userID: any) => {
-        console.log("Add user With ID: " + userID);
+    let [submitFriendRequest, {loading, error, data}] = useMutation(ADD_FRIEND_QUERY);
+    const [myUsername, setUsername] = useState('');
+
+    const addUser = async (username: String) => {
+        console.log("Submitting Friend Request");
+        try {
+            await submitFriendRequest({variables: {friendUsername: username}})
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    //Get my username from secure store
+    (async () => {
+        //Get authPayload from local storage
+        let authPayload = await SecureStore.getItemAsync('authPayload');
+        // Convert the Json string to a Json object
+        if (authPayload === null) {
+            return
+        }
+        const authPayloadObject: AuthPayload = JSON.parse(authPayload);
+        setUsername(authPayloadObject.login.user.username);
+    })();
+
+    let [acceptFriendRequest, {
+        loading: loadingAcceptFR,
+        error: errorAcceptFR,
+        data: dataAcceptFR
+    }] = useMutation(ACCEPT_FRIEND_REQUEST_QUERY);
+
+    const acceptRequest = async (username: String) => {
+        console.log("Accepting Friend Request");
+
+        try {
+            await acceptFriendRequest({variables: {friendUsername: username}})
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     return (
@@ -16,9 +57,35 @@ const ProfileSearchBox = ({userID, username}: any) => {
                 <Text style={profileSearchBox.pfName}>{username}</Text>
             </View>
             <View style={profileSearchBox.pfAddContainer}>
-                <TouchableOpacity style={profileSearchBox.pfAddButton} onPress={() => addUser(userID)}>
-                    <Image style={profileSearchBox.pfAddButtonImg} source={require("../assets/icons/addUser.png")}/>
-                </TouchableOpacity>
+                <View style={profileSearchBox.pfAddButton}>
+
+                    {loading && <ActivityIndicator size="small" color="#ffffff"/>}
+                    {error && <Text style={profileSearchBox.pfAddButtonText}>Error</Text>}
+                    {data && <Text style={profileSearchBox.pfAddButtonText}>Added</Text>}
+                    {friendRequestStatus.status !== 'Undefined'
+                        && friendRequestStatus.status !== 'Accepted'
+                        && friendRequestStatus.needToAcceptBy !== myUsername
+                        && !data
+                        && <Text style={profileSearchBox.pfAddButtonText}>{friendRequestStatus.status}</Text>}
+
+                    {friendRequestStatus.status === 'Accepted' && <Image style={profileSearchBox.pfAddButtonImg}
+                                                                         source={require("../assets/icons/userAdded.png")}/>}
+
+                    {friendRequestStatus.status === 'Undefined' && !data &&
+                        <TouchableOpacity onPress={() => addUser(username)} style={profileSearchBox.pfAddButtonB}>
+                            <Image style={profileSearchBox.pfAddButtonImg}
+                                   source={require("../assets/icons/addUser.png")}/>
+                        </TouchableOpacity>}
+
+                    {!dataAcceptFR && friendRequestStatus.needToAcceptBy === myUsername && !data &&
+                        <TouchableOpacity onPress={() => acceptRequest(username)}
+                                          style={profileSearchBox.pfAddButtonB}>
+                            <Text style={profileSearchBox.pfAddButtonText}>ACCEPT</Text>
+                        </TouchableOpacity>}
+
+                    {dataAcceptFR && <Text style={profileSearchBox.pfAddButtonText}>Accepted</Text>}
+
+                </View>
             </View>
         </View>
     )
