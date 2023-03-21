827c0bf59309619fd80dd77d06565cdd73ceba31
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-21 01:06:02 +0100

    sending a chat now has loading animation

--- components/ChatSend.tsx
+++ components/ChatSend.tsx
@@ -1,14 +1,16 @@
-import {Text, TouchableHighlight, View} from "react-native";
+import {ActivityIndicator, Text, TouchableHighlight, View} from "react-native";
 import stylesChatSend from "../styles/chatSend";
+import React from "react";
 
 
-export function ChatSend({msg, date}: { msg: string, date: string }) {
+export function ChatSend({msg, date, loading}: { msg: string, date: string, loading: boolean }) {
     return (
         <TouchableHighlight activeOpacity={0.6} underlayColor="#DDDDDD" style={stylesChatSend.rightSide}>
             <View style={stylesChatSend.MsgBox}>
                 <View style={stylesChatSend.textContent}>
                     <View style={stylesChatSend.textBubble}>
-                        <Text style={stylesChatSend.textBubblePadding}>{msg}</Text>
+                        <Text style={stylesChatSend.textBubblePadding}>{msg} {loading &&
+                            <ActivityIndicator size="small" color="#ffffff"/>}</Text>
                     </View>
                 </View>
             </View>
