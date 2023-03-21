ad2d75382f3af8949ab86ea57e04f58170cd3c0b
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-21 01:02:04 +0100

    MsgRoom.tsx now encrypts and decrypts messages send

--- Pages/MsgRoom.tsx
+++ Pages/MsgRoom.tsx
@@ -4,18 +4,29 @@ import {useNavigation} from "@react-navigation/native";
 import {ChatReceive} from "../components/ChatReceive";
 import {ChatSend} from "../components/ChatSend";
 import {LOAD_CHATROOM_CONTENT} from "../constants/graphql/querys/loadChatRoomQuery";
-import {useQuery, useSubscription} from "@apollo/client";
+import {useMutation, useQuery, useSubscription} from "@apollo/client";
 import {FlatList} from "react-native-gesture-handler";
 import {CHAT_ROOM_SUB} from "../constants/graphql/querys/chatRoomSub";
-import {useState} from "react";
+import {SetStateAction, useEffect, useState} from "react";
+import {SEND_MSG_MUT} from "../constants/graphql/querys/sendMsgMut";
+import {box} from "tweetnacl";
+import {getPrivateKeyPerUser} from "../Functions/storePrivateKeyPerUser";
+import {decode as decodeBase64} from "@stablelib/base64";
+import {decrypt, encrypt} from "../Functions/crypto";
 
 export function MsgRoom({route}: any) {
     let navigation = useNavigation();
 
     const nameOfUser = route.params.nameOfUser;
     const chatRoomId = route.params.chatRoomId;
+    const userInfos = route.params.userInfo
+
 
     let [combinedData, setCombinedData] = useState<any[]>([]);
+    //message to send
+    let [msg, setMsg] = useState("");
+    let [encryptedMsg, setEncryptedMsg] = useState('');
+    let [secretKey, setSecretKey] = useState(new Uint8Array(32));
 
 
     //Get chatRoomContent from API with chatRoomId Query= LOAD_CHATROOM_CONTENT
@@ -24,7 +35,8 @@ export function MsgRoom({route}: any) {
         onCompleted: (data) => {
             //Set the data to the combined data
             setCombinedData(data.loadChatContent);
-        }
+        },
+        fetchPolicy: "no-cache"
     });
 
     //Subscribe to the chatRoomSub with chatRoomId
@@ -32,12 +44,70 @@ export function MsgRoom({route}: any) {
         variables: {chatId: chatRoomId},
         onData: (data) => {
             //If the data is not null, add the new message to the combinedData
-            console.log(data.data.data.chatRoomContent);
             if (data.data.data.chatRoomContent !== null) {
+                //Check if I am the sender of the message if so, do not add it to the combinedData
+                if (data.data.data.chatRoomContent.receiver === nameOfUser) {
+                    return;
+                }
+
                 setCombinedData((combinedData) => [data.data.data.chatRoomContent, ...combinedData]);
             }
         }
     });
+
+    // @ts-ignore
+    useEffect(() => {
+        async function fetchData() {
+            // @ts-ignore
+            const loggedInUserId = global.LOGGED_IN_USER._id;
+            const publicKey: Uint8Array = decodeBase64(userInfos.publicKey);
+            const privateKey: Uint8Array = decodeBase64(await getPrivateKeyPerUser(loggedInUserId) as string);
+
+            const secretKey: Uint8Array = box.before(publicKey, privateKey);
+            setSecretKey(secretKey);
+        }
+
+        fetchData();
+    }, []);
+
+
+    //Send message to the chatRoom
+    const [sendMsg, {loading: loadingSend, error: errorSend, data: dataSend}] = useMutation(SEND_MSG_MUT, {
+        variables: {chatId: chatRoomId, message: encryptedMsg, receiver: nameOfUser},
+    });
+
+    const sendMsgToChatRoom = () => {
+
+        //Encrypt the message with the secretKey
+        const encryptedMessage = encrypt(secretKey, msg)
+
+        sendMsg({
+            variables: {chatId: chatRoomId, message: encryptedMessage, receiver: nameOfUser}
+        });
+
+        //Create a chat message object
+        let chatMessage = {
+            message: msg,
+            messageTime: new Date().toLocaleString(),
+            receiver: nameOfUser,
+            // @ts-ignore
+            sender: global.LOGGED_IN_USER
+        }
+
+        setCombinedData((combinedData) => [chatMessage, ...combinedData]);
+        //Clear the message input
+        setMsg("");
+
+    }
+
+    let textInputProps = {
+        placeholder: "Type a message",
+        placeholderTextColor: "#fff",
+        style: stylesMsgRoom.msgInput,
+        onChangeText: (text: SetStateAction<string>) => setMsg(text),
+        value: msg,
+        width: msg.trim() !== "" ? "80%" : "100%",
+    }
 
 
     const goBack = () => {
@@ -49,51 +119,70 @@ export function MsgRoom({route}: any) {
 
 
     return (
-
         <View style={stylesMsgRoom.containerMain}>
-            <View style={stylesMsgRoom.head}></View>
-            <View style={stylesMsgRoom.container}>
-                <View style={stylesMsgRoom.spacer}></View>
-                <View style={stylesMsgRoom.header}>
-                    <TouchableOpacity style={{justifyContent: "center"}} onPress={goBack}>
-                        <Text style={[{marginRight: "5%"}, stylesMsgRoom.detailsText]}>Back</Text>
-                    </TouchableOpacity>
-                    <View style={stylesMsgRoom.nameOfUser}><Text
-                        style={stylesMsgRoom.nameOfUserText}>{nameOfUser}</Text></View>
-                    <View style={stylesMsgRoom.bubbleButtonRight}></View>
-                </View>
+            <KeyboardAvoidingView behavior="position">
+                <View style={stylesMsgRoom.headerClear}></View>
                 <ImageBackground source={require('../assets/img/BackGroundChatRoom7.png')}
                                  style={stylesMsgRoom.background}>
                     <FlatList
                         inverted={true}
                         data={combinedData}
                         renderItem={({item}) => {
+                            let decryptedMessage = "";
+                            try {
+                                decryptedMessage = decrypt(secretKey, item.message)
+                            } catch (e) {
+                                decryptedMessage = item.message
+                            }
+
                             if (item.receiver === nameOfUser) {
                                 //This means that the message was sent by me
-                                return <ChatSend msg={item.message} date={item.messageTime}/>
+                                //only send loading if the message is the last one
+                                if (combinedData[0].message === item.message) {
+                                    return <ChatSend msg={decryptedMessage} date={item.messageTime}
+                                                     loading={loadingSend}/>
+                                }
+
+                                return <ChatSend msg={decryptedMessage} date={item.messageTime} loading={false}/>
                             } else {
                                 //This means that the message was sent by the other user
-                                return <ChatReceive msg={item.message} date={item.messageTime}/>
+                                return <ChatReceive msg={decryptedMessage} date={item.messageTime}/>
                             }
                         }}
+                        keyboardDismissMode={"on-drag"}
                     >
                     </FlatList>
                     <View style={stylesMsgRoom.footerClear}></View>
                 </ImageBackground>
-            </View>
-            <KeyboardAvoidingView behavior="position">
                 <View style={stylesMsgRoom.footer}>
                     <View style={stylesMsgRoom.container}>
                         <View style={stylesMsgRoom.msgInputContainer}>
-                            <TextInput style={stylesMsgRoom.msgInput}></TextInput>
-                            <TouchableOpacity style={stylesMsgRoom.sendButton}>
-                                <Image style={stylesMsgRoom.sendButtonImg}
-                                       source={require("../assets/icons/send.png")}/>
-                            </TouchableOpacity>
+                            <TextInput {...textInputProps}></TextInput>
+                            {msg.trim() !== "" ?
+                                <TouchableOpacity style={stylesMsgRoom.sendButton} onPress={() => sendMsgToChatRoom()}>
+                                    <Image style={stylesMsgRoom.sendButtonImg}
+                                           source={require("../assets/icons/send.png")}/>
+                                </TouchableOpacity> : null}
                         </View>
                     </View>
                 </View>
             </KeyboardAvoidingView>
+
+            {/* Preventing the header from moving up when the keyboard is open */}
+            {/* that's why it's outside the KeyboardAvoidingView and underneath */}
+            <View style={stylesMsgRoom.head}>
+                <View style={stylesMsgRoom.container}>
+                    <View style={stylesMsgRoom.spacer}></View>
+                    <View style={stylesMsgRoom.header}>
+                        <TouchableOpacity style={{justifyContent: "center"}} onPress={goBack}>
+                            <Text style={[{marginRight: "5%"}, stylesMsgRoom.detailsText]}>Back</Text>
+                        </TouchableOpacity>
+                        <View style={stylesMsgRoom.nameOfUser}><Text
+                            style={stylesMsgRoom.nameOfUserText}>{nameOfUser}</Text></View>
+                        <View style={stylesMsgRoom.bubbleButtonRight}></View>
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
