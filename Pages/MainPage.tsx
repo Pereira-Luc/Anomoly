fb178a0837956b8f55b2c087a1c49b50bd4a471f
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-27 17:51:51 +0200

    Send push notification token if it is missing on the server

--- Pages/MainPage.tsx
+++ Pages/MainPage.tsx
@@ -4,15 +4,54 @@ import Chats from "../components/Chats";
 import Groups from "../components/Groups";
 import Settings from "../components/Settings";
 
-import React from "react";
+import React, {useEffect} from "react";
 import {ActionSheetProvider} from "@expo/react-native-action-sheet";
 import {Host} from "react-native-portalize";
+import {useLazyQuery, useMutation} from "@apollo/client";
+import {registerForPushNotificationsAsync} from "../Functions/sendPusNotification";
+import {GET_PUSH_NOTIFICATION_TOKEN, SAVE_PUSH_NOTIFICATION_TOKEN} from "../constants/graphql/querys/pushNotification";
 
 const MainPage = () => {
     //Default page is Chats
     const [page, setPage] = React.useState("Chats");
 
+    //lazy query to send pushNotificationToken to server
+    const [sendPushNotificationToken] = useMutation(SAVE_PUSH_NOTIFICATION_TOKEN, {
+        onCompleted: (data) => {
+            console.log(data);
+        }
+    });
+
     //TODO: Add a useEffect to check if the user is logged in, if not, redirect to the start page
+
+    //TODO: Check if server already has my pushNotificationToken, if not, send it to the server
+    const [checkPushNotificationToken, {error}] = useLazyQuery(GET_PUSH_NOTIFICATION_TOKEN, {
+        onCompleted: async (data) => {
+            console.log(data);
+            //If not asked for permission, data is null
+            if (!data.checkIfPushNotificationIsEnabled) {
+                const pushNotificationToken = await registerForPushNotificationsAsync()
+                sendPushNotificationToken({
+                    variables: {token: pushNotificationToken}
+                });
+            }
+        },
+        fetchPolicy: "no-cache"
+    });
+
+    (useEffect(() => {
+        const checkPushNotificationTokenAsync = async () => {
+            console.log("checkPushNotificationTokenAsync");
+            const token = await registerForPushNotificationsAsync()
+            if (token) {
+                checkPushNotificationToken();
+                return
+            }
+
+            console.log("No Permission for Push Notifications");
+        }
+
+    }))
 
     return (
         <Host>
