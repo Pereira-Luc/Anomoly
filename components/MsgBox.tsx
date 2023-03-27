85d8593117db9ed9db238cee7005582f44255138
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-27 17:52:14 +0200

    add profile picture to chat feed

--- components/MsgBox.tsx
+++ components/MsgBox.tsx
@@ -2,12 +2,17 @@ import {Image, Text, TouchableOpacity, View} from "react-native";
 import stylesMsgBox from "../styles/stylesMsgBox";
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import {useNavigation} from "@react-navigation/native";
+import {manipulateAsync, SaveFormat} from "expo-image-manipulator";
+import {useEffect, useState} from "react";
 //https://docs.swmansion.com/react-native-gesture-handler/docs/api/components/swipeable/
 
 // Maybe interesting for later: https://docs.swmansion.com/react-native-gesture-handler/docs/api/components/drawer-layout
 
 export function MsgBox({lastMsg, nameOfUser, date, chatId, userInfo}: String | any) {
     let navigation = useNavigation();
+
+    const [profilePic, setProfilePic] = useState<string>('');
+
     const onDelete = () => {
         console.log("Delete");
     }
@@ -23,11 +28,33 @@ export function MsgBox({lastMsg, nameOfUser, date, chatId, userInfo}: String | a
         console.log("Open chat");
         //Navigate to MsgRoom
         // @ts-ignore
-        navigation.navigate("MsgRoom", {chatRoomId: chatId, nameOfUser: nameOfUser, userInfo: userInfo});
+        navigation.navigate("MsgRoom", {
+            chatRoomId: chatId,
+            nameOfUser: nameOfUser,
+            userInfo: userInfo,
+            profileImageUri: profilePic
+        });
 
         //get UserInfos
-
     }
+
+    async function base64ToImage(base64String: string, width: number) {
+        const {uri} = await manipulateAsync(
+            `data:image/jpeg;base64,${base64String}`,
+            [{resize: {width}}],
+            {format: SaveFormat.JPEG, compress: 1}
+        );
+
+        return uri;
+    }
+
+    useEffect(() => {
+        base64ToImage(userInfo.profilePic, 500).then((uri) => {
+            setProfilePic(uri);
+        }).catch((e) => {
+            setProfilePic(require("../assets/icons/profile.png"));
+        });
+    }, []);
 
     return (
         <Swipeable
@@ -37,7 +64,8 @@ export function MsgBox({lastMsg, nameOfUser, date, chatId, userInfo}: String | a
             }>
             <TouchableOpacity style={stylesMsgBox.MsgBox} onPress={openChat}>
                 <View style={stylesMsgBox.pfPic}>
-                    <Image style={stylesMsgBox.pfPicImg} source={require("../assets/icons/profile.png")}/>
+                    {profilePic ? <Image style={stylesMsgBox.pfPicImg} source={{uri: profilePic}}/> :
+                        <Image style={stylesMsgBox.pfPicImg} source={require('../assets/icons/profile.png')}/>}
                 </View>
                 <View>
                     <Text style={stylesMsgBox.nameOfUser}>{nameOfUser}</Text>
