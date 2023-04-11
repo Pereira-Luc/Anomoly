e3ae9c550ffea701b94811ecb44cdf9f2e85c376
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2023-04-11 18:13:45 +0200

    getTimeFormat now shows "now" if diffInMinutes < 1

--- Functions/functions.ts
+++ Functions/functions.ts
@@ -1,5 +1,7 @@
 //Format Date Shows time < 1h in minutes and > 1h in hours
 //Time > 24h in days and > 7d in weeks and > 4w in months
+import {manipulateAsync, SaveFormat} from "expo-image-manipulator";
+
 export const getTimeFormat = (date: Date) => {
     let d = new Date(date)
     let now = new Date()
@@ -9,6 +11,10 @@ export const getTimeFormat = (date: Date) => {
     let diffInDays = diff / (1000 * 3600 * 24)
     let diffInWeeks = diff / (1000 * 3600 * 24 * 7)
     let diffInMonths = diff / (1000 * 3600 * 24 * 7 * 4)
+    //if less than 1 minute show "now"
+    if (diffInMinutes < 1) {
+        return "now"
+    }
     if (diffInHours < 1) {
         return Math.round(diffInMinutes) + "m"
     }
@@ -22,4 +28,15 @@ export const getTimeFormat = (date: Date) => {
         return Math.round(diffInWeeks) + "w"
     }
     return Math.round(diffInMonths) + "m"
+}
+
+
+export async function base64ToImage(base64String: string, width: number) {
+    const {uri} = await manipulateAsync(
+        `data:image/jpeg;base64,${base64String}`,
+        [{resize: {width}}],
+        {format: SaveFormat.JPEG, compress: 1}
+    );
+
+    return uri;
 }
