73139f42889045d68f5b35ae1cf1e9f35eb9894c
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2023-04-25 15:14:18 +0200

    query to get profile picture of a User

--- constants/graphql/querys/getProfileImg.ts
+++ constants/graphql/querys/getProfileImg.ts
@@ -3,8 +3,6 @@ import {gql} from "@apollo/client";
 
 export const GET_USER_PROFILE_IMG = gql`
     query getProfileImageUri($userId: ID!) {
-        getUserInformation(userId: $userId) {
-            profilePic
-        }
+        getUserProfilePic(userId: $userId)
     }
 `
