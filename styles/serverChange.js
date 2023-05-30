fe38ee0dc2741a29226a74a9b607e94df3214dd1
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2023-05-30 22:15:52 +0200

    new change server page

--- styles/serverChange.js
+++ styles/serverChange.js
@@ -1,26 +1,55 @@
-import {StyleSheet} from "react-native";
+import {Dimensions, Platform, StyleSheet} from "react-native";
 import {colors} from "./colors/colors";
 
 export default StyleSheet.create({
     container: {
-        position: 'absolute',
         backgroundColor: colors.primaryBackground,
-        height: '117%',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+
+    },bottomSheetStyle: {
+        backgroundColor: 'rgba(38,38,38,0)',
+    },currenServerBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        verticalAlign: 'center',
+        backgroundColor: colors.primaryBackgroundDark,
+        height: Platform.OS === 'ios' ? '30%' : '40%',
+        width: '100%',
+        borderRadius: 20,
+        padding: 20,
+        marginTop: 20,
+    },serverChangeInputBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        verticalAlign: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.primaryBackgroundDark,
+        height: '70%',
         width: '100%',
         borderRadius: 20,
         padding: 10,
-        top: -40,
-        shadowOpacity: 0.8,
-        shadowRadius: 5,
-        shadowColor: colors.primaryDetail,
+        marginTop: 20,
+        marginBottom: 20,
+    },
+    changeServerBox: {
+        backgroundColor: colors.primaryBackground,
+        height: '50%',
+        width: '90%',
+        borderRadius: 20,
+        padding: 10,
     },
     serverChangeText: {
         color: '#ffffff',
-        fontSize: 20,
+        fontSize: Platform.OS === 'ios' ? 20 : 15,
     },
     serverChangeTextHeader: {
         color: '#ffffff',
-        fontSize: 20,
+        fontSize: Platform.OS === 'ios' ? 30 : 20,
         marginTop: 10,
         width: '100%',
         textAlign: 'center',
@@ -36,7 +65,7 @@ export default StyleSheet.create({
         borderRadius: 20,
     },serverChangeInput: {
         color: '#ffffff',
-        fontSize: 20,
+        fontSize: Platform.OS === 'ios' ? 20 : 15,
         backgroundColor: 'rgba(255, 255, 255, 0.2)',
         width: '100%',
         height: 40,
@@ -49,18 +78,22 @@ export default StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         width: '100%',
-        height: 40,
+        height: (Platform.OS === 'ios') ? 50 : 40,
         backgroundColor:colors.primaryDetail,
         borderRadius: 10,
         marginRight: 5,
+        marginTop: 10,
     },serverChangeButtonText: {
-        color: '#000000',
-        fontSize: 20,
+        color: 'white',
+        fontSize: Platform.OS === 'ios' ? 20 : 15,
         width: '100%',
         textAlign: 'center',
+    },serverChangeButtonReset: {
+        color: '#000000',
+        backgroundColor: colors.primaryDetailWarning,
     },serverChangeHeader: {
         width: '100%',
-        height: 40,
+        height: Platform.OS === 'ios' ? 50 : 40,
         borderRadius: 10,
         top: 0,
     },serverChangeBody: {
@@ -73,12 +106,13 @@ export default StyleSheet.create({
         top: -50,
     },serverChangeTextB: {
         paddingTop: 30,
-        padding: 20,
+        padding: Platform.OS === 'ios' ? 10 : 5,
         paddingLeft: 0,
         color: '#ffffff',
-        fontSize: 15,
+        fontSize: Platform.OS === 'ios' ? 15 : 13,
         textAlign: 'left',
         width: '100%',
+        textAlign: 'center',
     }
 
 });
